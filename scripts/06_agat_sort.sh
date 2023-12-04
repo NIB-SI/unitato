767c3027b75c8f4ac744fcfa53e9994ee267b0ed
zagorGit <maja.zagorscak@nib.si>
2023-12-04 18:41:01 +0100

    Update 06_agat_sort.sh

--- scripts/06_agat_sort.sh
+++ scripts/06_agat_sort.sh
@@ -5,3 +5,7 @@ conda update agat
 # https://agat.readthedocs.io/en/latest/topological-sorting-of-gff-features.html
 agat_convert_sp_gxf2gxf.pl --gff pure_addition_flank0.gff  > sorted_pure_addition_flank0.gff;
 # repeat for all GFFs
+
+# cob+nvert GFF to GTF
+agat_convert_sp_gff2gtf.pl --gff Unitato.gff -o Unitato.gtf
+
